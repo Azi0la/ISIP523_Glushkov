5135ad4bd425261018af0ad31b33ad1f2e3d81f0
Aziola <gluskov934@gmail.com>
2026-04-07 11:50:29 +0300

    готово.

--- Pr_PC_Builder/Pages/PartPage.xaml.cs
+++ Pr_PC_Builder/Pages/PartPage.xaml.cs
@@ -21,10 +21,21 @@ namespace Pr_PC_Builder.Pages
     public partial class PartPage : Page
     {
         public List <basepart_> allparts = Core.Context.basepart_.ToList ();
+        public static List<basepart_> pageParts = new List<basepart_>();
+        public static List<string> manufacturers = new List<string>() { "Все" };
         public PartPage(parttype_ prt)
         {
             InitializeComponent();
-            TypeList.ItemsSource = allparts.Where(c => c.parttypeid == prt.id).ToList();
+            manufacturers = new List<string>() { "Все" };
+            pageParts = allparts.Where(c => c.parttypeid == prt.id).ToList();
+            TypeList.ItemsSource = pageParts;
+            foreach (basepart_ item in pageParts)
+            {
+                manufacturers.Add(item.manufacturer_.name);
+            }
+            manufacturers = manufacturers.Distinct().ToList();
+            ManufactCB.ItemsSource = manufacturers;
+            ManufactCB.SelectedIndex = 0;
 
         }
 
@@ -50,6 +61,27 @@ namespace Pr_PC_Builder.Pages
             {
                 NavigationService.Navigate(new MainPage());
             }
+        }
+
+        private void ManufactCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateData();
+        }
+
+        private void SearchTB_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateData();
+        }
+
+        public void UpdateData()
+        {
+            List<basepart_> Filtered = pageParts;
+            Filtered = Filtered.Where(p => p.name.ToLower().Contains(SearchTB.Text.ToLower())).ToList();
+            if (ManufactCB.SelectedItem as string != "Все")
+            {
+                Filtered = Filtered.Where(p => p.manufacturer_.name.Contains(ManufactCB.SelectedItem as string)).ToList();
+            }
+            TypeList.ItemsSource = Filtered;
         }
     }
 }
